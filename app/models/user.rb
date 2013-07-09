b8b980338357341c08147b5697f588ba15e0b263
Tim <tim.blonski@gmail.com>
2013-07-09 13:51:19 -0400

    merging updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name
+
+  validates_presence_of :first_name, :last_name
+
+  def gender
+    self[:gender] || 'Not specified'
+  end
+
 end
